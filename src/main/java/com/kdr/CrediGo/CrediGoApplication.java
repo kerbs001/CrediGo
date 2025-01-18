b2663c5c68f070051368d6e515dea82d51c484a1
Kerby <rubinas.kerby@gmail.com>
2025-01-18 17:59:25 +0800

    Initialized CrediGo project

---
+++ src/main/java/com/kdr/CrediGo/CrediGoApplication.java
@@ -0,0 +1,13 @@
+package com.kdr.CrediGo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrediGoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrediGoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kdr/CrediGo/CrediGoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kdr.CrediGo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrediGoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
